4e7526f78013a00d614afa46f2192d30ae33b452
fabienwnklr <fabien.winkler@outlook.fr>
2024-08-18 16:29:36 +0200

    chore: create model and migrations

---
+++ app/models/trackday.ts
@@ -0,0 +1,34 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany, hasOne } from '@adonisjs/lucid/orm'
+import Track from './track.js'
+import type { HasMany, HasOne } from '@adonisjs/lucid/types/relations'
+import Chrono from './chrono.js'
+
+export default class Trackday extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column.dateTime({ autoCreate: false })
+  declare date: DateTime
+
+  @column()
+  declare track_map: string
+
+  @column()
+  declare weather: string
+
+  @column()
+  declare tire_pressure: string
+
+  @hasOne(() => Track)
+  declare track: HasOne<typeof Track>
+
+  @hasMany(() => Chrono)
+  declare chronos: HasMany<typeof Chrono>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ database/migrations/1723991108527_create_chronos_table.ts
@@ -0,0 +1,18 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'chronos'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('track_id').unsigned().references('track.id').onDelete('CASCADE') // delete post when user is deleted
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
